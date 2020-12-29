4e0b6d395e21a3e6e79523429f42530f1c63b6a2
caohaitao.cc <caohaitao.cc@bytedance.com>
2020-12-29 16:58:58 +0800

    fix: add flow

---
+++ lodash-test/flow.ts
@@ -0,0 +1,32 @@
+/*
+ * @Author: Cao Haitao
+ */
+/*
+ * @Author: Cao Haitao
+ */
+import * as _ from 'lodash';
+
+_.flow();
+
+type TFunc = (...args:any[])=>any;
+
+function flow(...func:TFunc[]):TFunc{
+    let len = func.length;
+    let index = len-1;
+    while (index--) {
+        if (typeof func[index] !== 'function') {
+          throw new TypeError('Expected a function')
+        }
+    }
+    return function(...args){
+        let idx = 0;
+        let result = length ?  func[index].call(this, args) : args[0];
+
+        while(++idx<length){
+            result = func[index].call(this,result)
+        }
+
+        return result
+
+    }
+}
