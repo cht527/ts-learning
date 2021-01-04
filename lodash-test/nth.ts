d2d77af17495f1122aeabfee526e6bab4b02e09a
caohaitao.cc <caohaitao.cc@bytedance.com>
2021-01-04 15:05:38 +0800

    feat: add nth

---
+++ lodash-test/nth.ts
@@ -0,0 +1,20 @@
+/*
+ * @Author: Cao Haitao
+ */
+/*
+ * @Author: Cao Haitao
+ */
+
+
+
+
+function nth<T>(arr:Array<T>, n?:number) {
+    const _n = typeof n === 'number' ? (n < 0 ? n + arr.length : n) : 0;
+
+    return arr[_n]
+}
+
+const testNthArr = [1,3];
+
+console.log(nth(testNthArr,1));
+
