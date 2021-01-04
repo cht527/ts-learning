c777b09684a112af08a3751e6c7886ee778e3ae8
caohaitao.cc <caohaitao.cc@bytedance.com>
2021-01-04 15:08:37 +0800

    feat: nth

--- js/lodash-test/nth.js
+++ js/lodash-test/nth.js
@@ -6,6 +6,10 @@
  * @Author: Cao Haitao
  */
 function nth(arr, n) {
+    const len = Array.isArray(arr) ? arr.length : 0;
+    if (!len) {
+        return;
+    }
     const _n = typeof n === 'number' ? (n < 0 ? n + arr.length : n) : 0;
     return arr[_n];
 }
--- lodash-test/nth.ts
+++ lodash-test/nth.ts
@@ -9,6 +9,12 @@
 
 
 function nth<T>(arr:Array<T>, n?:number) {
+    const len = Array.isArray(arr) ? arr.length :0
+
+    if(!len){
+        return;
+    }
+    
     const _n = typeof n === 'number' ? (n < 0 ? n + arr.length : n) : 0;
 
     return arr[_n]
